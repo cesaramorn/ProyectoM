d104c2da50adae78e0732a105babf2e078e0e39d
cesaramorn <cesar.amor.n@gmail.com>
2026-05-05 13:29:21 +0200

    Clean up CSS redundancy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
 
-            {/* Private / Layout routes */}
+            {/* Private routes */}
             <Route element={<Layout />}>
               {/* Future routes */}
             </Route>
